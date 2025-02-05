b63ea9f88e74e1886f41edc91dcffb763010aee3
azfang <47017488+azfang@users.noreply.github.com>
2025-02-04 23:58:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
-
-    /*! Fades in page on load */
-    // $('.button').css('display', 'none');
     $('body').hide().delay(300).fadeIn(500);
-    // $('#textline1').hide();
-    $('.textline1').delay(8000).fadeOut(2000);
-    // $('.textline1').hide().delay(3000).fadeIn(1000);
-    // $('.button').hide().delay(300).fadeIn(500);
-    
     });
 
 var ppbutton = document.getElementById("playVideo");
